fd829ab4491e6821389465c773e0ea3c9f8c9c62
김효린 <hyorin97@naver.com>
2021-08-17 21:28:58 +0900

    first commit with gitignore file (#1)

---
+++ app/src/main/java/com/euphonyforever/looking_for_job/MainActivity.java
@@ -0,0 +1,38 @@
+package com.euphonyforever.looking_for_job;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import euphony.lib.transmitter.EuDataEncoder;
+import euphony.lib.transmitter.EuTxManager;
+import euphony.lib.receiver.AcousticSensor;
+import euphony.lib.receiver.EuRxManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button buttonSend;
+    Button buttonReceive;
+
+    EuTxManager mTxManager = new EuTxManager() ;
+    EuDataEncoder mSpeakText ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonSend = findViewById(R.id.buttonSend) ;
+
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), sendResumeActivity.class) ;
+                startActivity(intent) ;
+            }
+        }) ;
+    }
+}
---
+++ app/src/main/java/com/euphonyforever/looking_for_job/sendResumeActivity.java
@@ -0,0 +1,14 @@
+package com.euphonyforever.looking_for_job;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sendResumeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_resume);
+    }
+}
