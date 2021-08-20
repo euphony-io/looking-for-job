6f4813eb0ba3d05363c6f6961e3750a7294233c7
6loss0m <gsjang0807@gmail.com>
2021-08-20 23:30:34 +0900

    Revert "Add resume info ouput sction"
    This reverts commit 7e517b0415520c9ca209641fe66412f2dc0d1464.

--- app/src/main/java/com/euphonyforever/looking_for_job/MainActivity.java
+++ app/src/main/java/com/euphonyforever/looking_for_job/MainActivity.java
@@ -2,42 +2,13 @@ package com.euphonyforever.looking_for_job;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        btn = findViewById(R.id.sendButton);
-
-        String name, birth, number, address, career;
-        name = "euphony";
-        birth = "2021-08-07";
-        number = "010-1111-2222";
-        address = "Seoul";
-        career = "developer";
-
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), OutputResumeInfo.class);
-                intent.putExtra("name",name);
-                intent.putExtra("birth",birth);
-                intent.putExtra("number",number);
-                intent.putExtra("address",address);
-                intent.putExtra("career",career);
-                startActivity(intent);
-            }
-        });
-
-
     }
 }
